48cca7710c0f157fc2ee6303893e8707374960ed
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-06 14:45:50 -0800

    Add functionality to post to mongoDB

--- server.js
+++ server.js
@@ -10,8 +10,24 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+const Fruit = require("./models/fruit.js");
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
+});
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
 });
 
 app.listen(8000, () => {
